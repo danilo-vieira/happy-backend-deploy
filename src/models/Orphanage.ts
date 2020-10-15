fcaede8431ca6ff83ab944bf25b6397af385938d
Danilo Vieira <danilo_strvieira@hotmail.com>
2020-10-14 21:05:33 -0300

    Configurando deploy

---
+++ src/models/Orphanage.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany, JoinColumn } from "typeorm";
+
+import Image from './Image';
+
+@Entity('orphanages')
+export default class Orphanage {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  latitude: string;
+
+  @Column()
+  longitude: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  instructions: string;
+
+  @Column()
+  opening_hours: string;
+
+  @Column('boolean')
+  open_on_weekends: boolean;
+
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
+}
